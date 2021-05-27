e35286c8b10bbec316ef367b7d6909b094da6923
jianxing.hu <jianxing.hu@xtalpi.com>
2021-05-27 10:54:12 +0800

    fix: 🐛 fix import

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@ from .draw import *
 from .shape import *
 from .pbar import tqdm, tnrange, trange, NO_WHITE 
 from .pbar import in_jupyter
-from .norm import Normalizer
+from .norm import Normalizer, NORMALIZATIONS, Normalization
 from .pdb_ext import *
