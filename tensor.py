2ad9e29c0df20e76e0b7e3a90f53875334b88a58
jianxing.hu <jianxing.hu@xtalpi.com>
2021-06-21 15:49:00 +0800

    docs: ✏️ spmmsp

--- tensor.py
+++ tensor.py
@@ -8,9 +8,9 @@ __all__ = [
 
 
 def spmmsp(
-    sp1: torch.Tensor,
-    sp2: torch.Tensor
-) -> torch.Tensor:
+    sp1: torch.sparse.Tensor,
+    sp2: torch.sparse.Tensor
+) -> torch.sparse.Tensor:
     assert sp1.size(-1) == sp2.size(0) and sp1.is_sparse and sp2.is_sparse
     m = sp1.size(0)
     k = sp2.size(0)
