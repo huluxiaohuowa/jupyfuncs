eceb44658fb1338751dc37e48de5da8269c603ce
jianxing.hu <jianxing.hu@xtalpi.com>
2021-06-23 14:41:18 +0800

    feat: 🎸 add makedirs

--- glob.py
+++ glob.py
@@ -12,3 +12,19 @@ def recursive_glob(treeroot, pattern):
         goodfiles = fnmatch.filter(files, pattern)
         results.extend(os.path.join(base, f) for f in goodfiles)
     return results
+
+
+def makedirs(path: str, isfile: bool = False) -> None:
+    """Creates a directory given a path to either a directory or file.
+    If a directory is provided, creates that directory. If a file is provided (i.e. :code:`isfile == True`),
+    creates the parent directory for that file.
+
+
+    Args:
+        path (str): Path to a directory or file. 
+        isfile (bool, optional): Whether the provided path is a directory or file.Defaults to False.
+    """    
+    if isfile:
+        path = os.path.dirname(path)
+    if path != '':
+        os.makedirs(path, exist_ok=True)
