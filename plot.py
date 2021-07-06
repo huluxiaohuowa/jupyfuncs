f846a9768b7b85efb5566be3d2a833289de1a165
jianxing.hu <jianxing.hu@xtalpi.com>
2021-07-06 16:11:15 +0800

    feat: 🎸 metric curves

--- plot.py
+++ plot.py
@@ -1,7 +1,12 @@
+from os import path as osp
+
 import seaborn as sn
 import sklearn
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+
+from .glob import get_num_lines, str_from_line
 
 
 def accuracies_heat(y_true, y_pred, num_tasks):
@@ -15,3 +20,42 @@ def accuracies_heat(y_true, y_pred, num_tasks):
     plt.figure(figsize=(10, 10))
     sn.set(font_scale=1.4) 
     sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}, fmt='.2f')
+
+
+def get_metrics_curves(
+    base_dir,
+    ckpts,
+    num_points,
+    metric='accuracy',
+    log_file='metrics.log'
+):
+    data_dict = {}
+    for ckpt in ckpts:
+        log = osp.join(
+            base_dir,
+            ckpt,
+            log_file
+        )
+        if not osp.exists(log):
+            print(f"WARNING: not log for {ckpt}")
+            continue
+        data_dict[ckpt] = []
+        for line_id in range(get_num_lines(log)):
+            line = str_from_line(log, line_id).split('\t')
+            if len(line) == 3 and line[1].strip() == metric:
+                data_dict[ckpt].append(
+                    [int(line[0].strip()), float(line[2].strip())]
+                )
+            if line_id >= num_points - 1:
+                break
+    plt.figure(figsize=(10, 6), dpi=300)
+    plt.style.use('ggplot')
+    plt.title('Result Analysis')
+    for ckpt, points in data_dict.items():
+        points_array = np.array(points).T
+        plt.plot(points_array[0], points_array[1], label=ckpt)
+    plt.legend()
+    plt.xlabel('Training size')
+    plt.ylabel(metric)
+    plt.grid(True)
+    plt.show()
