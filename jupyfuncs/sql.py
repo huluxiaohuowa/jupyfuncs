af459277287e1660bbe7c72b019110f06210af72
jianxing.hu <jianxing.hu@xtalpi.com>
2021-11-10 09:16:06 +0800

    feat: 🎸 psycopg connect

---
+++ jupyfuncs/sql.py
@@ -0,0 +1,19 @@
+import psycopg
+
+
+def connect_by_infofile(info_file: str) -> psycopg.Connection:
+    """Create a postgres connection
+
+    Args:
+        info_file (str): 
+            the path of the connection info like
+            host=127.0.0.1 dbname=dbname port=5432 user=postgres password=lala
+
+    Returns:
+        psycopg.Connection: 
+            the connection instance should be closed after committing.
+    """
+    conn = psycopg.connect(
+        open(info_file).readline()
+    )
+    return conn
