099480e9fd376a67d7225dd1c12c39191c93c18e
jianxing.hu <jianxing.hu@xtalpi.com>
2021-12-29 11:30:44 +0800

    fix: 🐛 fix pool server

--- jupyfuncs/proxy.py
+++ jupyfuncs/proxy.py
@@ -1,13 +1,12 @@
 import requests
 
 
-def get_proxy():
-    return requests.get("http://172.20.0.9:5010/get/?type=https").json().get("proxy")
-
-
-def get_proxies(https=False):
-    http_proxy = requests.get("http://172.20.0.9:5010/get/").json().get("proxy")
-    https_proxy = requests.get("http://172.20.0.9:5010/get/?type=https").json().get("proxy")
+def get_proxies(
+    pool_server='http://172.20.0.9:5010',
+    https=False
+):
+    http_proxy = requests.get(f"{pool_server}/get/").json().get("proxy")
+    https_proxy = requests.get(f"{pool_server}/get/?type=https").json().get("proxy")
     if https:
         proxy_dict = {
             'http': f'http://{https_proxy}',
