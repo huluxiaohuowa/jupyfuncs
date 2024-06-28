c5a3242655d132089107a235e9f5fd4bea7dc980
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-06-28 05:36:35 +0000

    fix: 🐛 !!!!
    ✅ Closes: git push

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
-import versioneer
 
 setup(
     name="jupyfuncs",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     author="Jianxing Hu",
     author_email="hu.jx@outlook.com",
     description="A collection of functions for Jupyter notebooks",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/your_github_username/jupyfuncs",
+    url="https://github.com/huluxiaohuowa/jupyfuncs",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -18,4 +15,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    include_package_data=True,
+    setup_requires=['setuptools_scm']
 )
