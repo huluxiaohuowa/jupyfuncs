67dd7553830c1f6f89fc8bfcfe8021350bad5999
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-06-28 05:41:40 +0000

    fix: 🐛 shenjing

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name="jupyfuncs",
+    use_scm_version={
+        "local_scheme": "no-local-version",
+    },
+    setup_requires=['setuptools_scm'],
     author="Jianxing Hu",
     author_email="hu.jx@outlook.com",
     description="A collection of functions for Jupyter notebooks",
@@ -15,6 +20,4 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    include_package_data=True,
-    setup_requires=['setuptools_scm']
 )
