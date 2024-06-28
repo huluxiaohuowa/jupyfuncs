fe0ab489ec4b612b08d7ee5a096e9d07b1c963f0
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-06-28 05:45:58 +0000

    fix: 🐛 sdfsdfsdf

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
-import os
+import setuptools_scm
+
+def custom_version_scheme(version):
+    """A custom version scheme that avoids any additional local version identifiers."""
+    return version.tag.base_version if version.tag else '0.0.0'
 
 setup(
     name="jupyfuncs",
     use_scm_version={
-        "local_scheme": "no-local-version",
+        'local_scheme': 'no-local-version',
+        'version_scheme': custom_version_scheme,
+        'write_to': 'jupyfuncs/_version.py'
     },
-    setup_requires=['setuptools_scm'],
     author="Jianxing Hu",
     author_email="hu.jx@outlook.com",
     description="A collection of functions for Jupyter notebooks",
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/huluxiaohuowa/jupyfuncs",
+    url="https://github.com/your_github_username/jupyfuncs",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,4 +25,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    setup_requires=['setuptools_scm']
 )
