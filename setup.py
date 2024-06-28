79f483e317ae8761ebeab980e6b78ecb57921b6b
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-06-28 06:13:54 +0000

    fix: 🐛 sdfsdf

--- setup.py
+++ setup.py
@@ -1,26 +1,17 @@
 from setuptools import setup, find_packages
-import setuptools_scm
 
-# 自定义 version_scheme
-def custom_version_scheme(version):
-    """ 格式化版本号以排除任何开发后缀，并保持版本自动递增 """
-    # 使用 'dirty' 表示未提交的更改
-    if version.dirty:
-        raise ValueError("Please commit your changes or stash them before building.")
-    # 如果没有有效的标签，使用 '0.0.0' 作为基础版本号
+def clean_version(version):
+    """移除本地版本信息（如 `.dev` 后缀）"""
     base_version = version.tag.base_version if version.tag else '0.0.0'
-    # 检查是否有距离上一个标签的提交
-    if version.distance is None or version.distance == 0:
-        return base_version
-    else:
-        # 自动生成递增的版本号
-        return f"{base_version}.{version.distance}"
+    if version.distance:
+        return f"{base_version}.post{version.distance}"
+    return base_version
 
 setup(
     name="jupyfuncs",
     use_scm_version={
         "local_scheme": "no-local-version",
-        "version_scheme": custom_version_scheme,
+        "version_scheme": clean_version,
         "write_to": "jupyfuncs/_version.py"
     },
     author="Jianxing Hu",
