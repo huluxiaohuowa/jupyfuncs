1ae82ebfa021506529892b648a54a1cdbee30467
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-06-28 06:00:24 +0000

    fix: 🐛 sdfsdfsdf

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
+import setuptools_scm
+
+# 自定义 version_scheme
+def clean_version_scheme(version):
+    """只返回基于标签的版本号，不添加任何开发后缀。"""
+    return version.format_with("{tag}")
 
 setup(
     name="jupyfuncs",
-    use_scm_version=True,  # 简单使用 setuptools_scm
+    use_scm_version={
+        "local_scheme": "no-local-version",
+        "version_scheme": clean_version_scheme,
+        "write_to": "jupyfuncs/_version.py"
+    },
     author="Jianxing Hu",
     author_email="hu.jx@outlook.com",
     description="A collection of functions for Jupyter notebooks",
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/your_github_username/jupyfuncs",
+    url="https://github.com/huluxiaohuowa/jupyfuncs",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
